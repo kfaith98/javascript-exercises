2b7125c439f06024acd3d4488d3a301f37f6138f
Faith <151044592+kfaith98@users.noreply.github.com>
2025-05-06 19:17:55 +0800

    Complete exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    let result = [];
 
+    for (let i = s.length - 1; i >= 0; i--) {
+        result.push(s[i]);
+    }
+    return result.join('');
 };
 
 // Do not edit below this line
