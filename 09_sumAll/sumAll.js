363bb6d1b6632dea03237182796a9758139fa412
Faith <151044592+kfaith98@users.noreply.github.com>
2025-05-07 18:58:03 +0800

    Complete exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min <=0 || max <= 0) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+
+    let minimum = Math.min(min, max);
+    let maximum = Math.max(min, max);
+    let sum = 0;
+
+    for (let i = minimum; i <= maximum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
