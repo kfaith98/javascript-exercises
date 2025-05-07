667a7e98e2bd40eb991b27f48203a20ca32df056
Faith <151044592+kfaith98@users.noreply.github.com>
2025-05-07 18:07:22 +0800

    Complete exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...value) {
+    let newArray = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        if (!value.includes(array[i])) {
+            newArray.push(array[i]);
+        } 
+    }
+    return newArray;
 };
 
+/*
+const removeFromArray = function(array, ...value) {
+    for (let i = 0; i < array.length; i++) {
+        if (value.includes(array[i])) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
+};
+*/
 // Do not edit below this line
 module.exports = removeFromArray;
